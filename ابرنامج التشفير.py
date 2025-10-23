5b3deacaacc0aaa121dcc19c06c6302a0d669f3b
qwwpoo <145618750+qwwpoo@users.noreply.github.com>
2025-10-23 15:42:22 +0300

    Add files via upload

---
+++ ابرنامج التشفير.py
@@ -0,0 +1,13 @@
+import string
+word=''
+list = string.ascii_letters
+sant = input('enter the sentans:')
+scrit = int(input('enter the scrite number:'))
+for num in sant:
+ if num in list:
+  end=list.index(num)
+  new_ind=(end+scrit)%52     
+  word += list[new_ind]
+ else :
+  word += num 
+print(word) 
